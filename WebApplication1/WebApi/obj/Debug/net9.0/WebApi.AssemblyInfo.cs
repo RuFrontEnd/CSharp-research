d31a421a05a511f4f80c8115c4c19c584623e06b
Luke_Xie <k718198@gmail.com>
2026-01-06 21:00:17 +0800

    init project

--- WebApplication1/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ WebApplication1/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f82b39929b718e0487080bfbaf4144e27eb30d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6a9a4e619963e5887c1edf4d6f3c5fc5629626")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
